4a19e03619a95461dd7b1a805fc64514e63301ba
SPremakumar <premakumar.samya@gmail.com>
2026-08-25 13:44:41 +0200

    Configure Flask for Render

--- backend/app.py
+++ backend/app.py
@@ -90,4 +90,9 @@ def serve_react(path):
 
 # LANCEMENT
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",port=5000, debug=False)
+    # app.run(host="127.0.0.1",port=5000, debug=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
